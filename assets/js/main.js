983983e95e11f90eaa609d807ab061446947b041
dreamacting <tim@dreamacting.org>
2017-01-06 20:42:34 +0000

    -

--- assets/js/main.js
+++ assets/js/main.js
@@ -173,68 +173,23 @@
 
 })();
 
-/*
-	Tessellate by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
-(function($) {
-
-	skel.breakpoints({
-		wide: '(max-width: 1680px)',
-		normal: '(max-width: 1280px)',
-		narrow: '(max-width: 1000px)',
-		mobile: '(max-width: 736px)'
-	});
-
-	$(function() {
-
-		var	$window = $(window),
-			$body = $('body');
-
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
-
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
-
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
-
-		// Scrolly links.
-			$('.scrolly').scrolly();
-
-		// Prioritize "important" elements on narrow.
-			skel.on('+narrow -narrow', function() {
-				$.prioritize(
-					'.important\\28 narrow\\29',
-					skel.breakpoint('narrow').active
-				);
-			});
-
-	});
-
-})(jQuery);
-
 $("form").submit(function(e) {
 
-    var ref = $(this).find("[required]");
+ if (!e.target.checkValidity()) { 
+  console.log("I am Safari"); // Safari continues with form regardless of checkValidity being false
+  e.preventDefault(); // dismiss the default functionality
 
-    $(ref).each(function(){
-        if ( $(this).val() == '' )
+  $('#yourFormId :input:visible[required="required"]').each(function()
+    {
+        if(!this.validity.valid)
         {
-            alert("Required field should not be left blank.");
-
             $(this).focus();
-
-            e.preventDefault();
+            $(this).attr("placeholder", this.validationMessage).addClass('placeholderError');
+            $(this).val(''); // clear value so it shows error message on Placeholder.
             return false;
         }
-    });  return true;
-});
+    });
+    return; // its invalid, don't continue with submission
+}
+
+    e.preventDefault(); // have to add it again as Chrome, Firefox will neve see above
